dbf04856feaf58ffee056bdd098e4c3d176159e8
Manuel <you@example.com>
2024-05-21 19:23:14 -0500

    Ultimo update crude, api ON

--- src/main/java/com/Adminairbnb/AppAirBnB/controller/PropiedadController.java
+++ src/main/java/com/Adminairbnb/AppAirBnB/controller/PropiedadController.java
@@ -1,50 +1,54 @@
 package com.Adminairbnb.AppAirBnB.controller;
-
 import com.Adminairbnb.AppAirBnB.entity.Propiedad;
-import com.Adminairbnb.AppAirBnB.repository.PropiedadRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.Adminairbnb.AppAirBnB.service.PropiedadService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+
+@AllArgsConstructor
 @RestController
 @RequestMapping("/api/RegisterProperty")
 public class PropiedadController {
 
-    @Autowired
-    private PropiedadRepository private_PropiedadRepository;
-
+    private final PropiedadService propiedadService;
 
     @GetMapping
 
     public Iterable <Propiedad> list(){
-        return private_PropiedadRepository.findAll();
+        return propiedadService.findAll();
     }
+
+
     @GetMapping("{id}")
     public Propiedad get(@PathVariable Integer id) {
-        return private_PropiedadRepository
-                .findById(id)
-                .orElse(null);
-
-
+        return propiedadService.findById(id);
 
     }
+
+
+    @ResponseStatus (HttpStatus.CREATED)
     @PostMapping
     public Propiedad create(@RequestBody Propiedad propiedad) {
-        return private_PropiedadRepository.save(propiedad);
-
+        return propiedadService.create(propiedad);
     }
+
+
     @PutMapping ("{id}")
     public Propiedad update(@PathVariable Integer id,
                             @RequestBody Propiedad form) {
-        Propiedad propiedadFromDb = private_PropiedadRepository
-                .findById(id)
-                .orElse(null);
-        propiedadFromDb.setNombrePropiedad(form.getNombrePropiedad());
-        propiedadFromDb.setUbicacion(form.getUbicacion());
-        propiedadFromDb.setNumBaños(form.getNumBaños());
-        propiedadFromDb.setNumHabitaciones(form.getNumHabitaciones());
+
+        return propiedadService.update(id, form);
+
+    }
 
 
-        return private_PropiedadRepository.save(propiedadFromDb);
+
+    @ResponseStatus (HttpStatus.NO_CONTENT)
+    @DeleteMapping ("{id}")
+    public void delete(@PathVariable Integer id) {
+        propiedadService.delete(id);
+
 
     }
 }
---
+++ src/main/java/com/Adminairbnb/AppAirBnB/service/PropiedadService.java
@@ -0,0 +1,49 @@
+package com.Adminairbnb.AppAirBnB.service;
+
+import com.Adminairbnb.AppAirBnB.entity.Propiedad;
+import com.Adminairbnb.AppAirBnB.repository.PropiedadRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+
+
+public class PropiedadService {
+
+    private final PropiedadRepository propiedadRepository;
+
+    public Iterable <Propiedad> findAll(){
+        return propiedadRepository.findAll();
+    }
+
+    public Propiedad findById(Integer id) {
+        return propiedadRepository
+                .findById(id)
+                .orElse(null);
+    }
+
+    public Propiedad create(Propiedad propiedad) {
+        return propiedadRepository.save(propiedad);
+
+    }
+
+    public Propiedad update(Integer id, Propiedad form) {
+        Propiedad propiedadFromDb = findById(id);
+
+        propiedadFromDb.setNombrePropiedad(form.getNombrePropiedad());
+        propiedadFromDb.setUbicacion(form.getUbicacion());
+        propiedadFromDb.setNumBaños(form.getNumBaños());
+        propiedadFromDb.setNumHabitaciones(form.getNumHabitaciones());
+
+
+        return propiedadRepository.save(propiedadFromDb);
+
+    }
+    public void delete(Integer id) {
+        Propiedad propiedadFromDb = findById(id);
+        propiedadRepository.delete(propiedadFromDb);
+
+
+    }
+}
