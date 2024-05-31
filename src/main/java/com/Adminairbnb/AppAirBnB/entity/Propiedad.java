6295104612f6765ef65a3c5feab1749c00ac315e
Manuel <you@example.com>
2024-05-30 19:46:03 -0500

    Base de datos conectada

--- src/main/java/com/Adminairbnb/AppAirBnB/entity/Propiedad.java
+++ src/main/java/com/Adminairbnb/AppAirBnB/entity/Propiedad.java
@@ -1,33 +1,43 @@
 package com.Adminairbnb.AppAirBnB.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
-@Entity
-@Getter
-@Setter
-@RequiredArgsConstructor
+import java.io.Serializable;
+
+@Data
 @NoArgsConstructor
-public class Propiedad {
+@AllArgsConstructor
+@Entity(name="Propiedad")
+@Table(name="PROPIEDADES")
+public class Propiedad implements Serializable {
     @Id
-    @GeneratedValue (strategy = GenerationType.AUTO) //id creados
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "propiedad_seq")
+    @SequenceGenerator(name = "propiedad_seq", sequenceName = "propiedad_sequence", allocationSize = 1)
+    @Column(name= "ID_PROPIEDAD", nullable = false, unique = true)
     private Integer id;
 
-    @NonNull
+    @Column(name = "NOMBRE_PROPIEDAD", nullable = false)
     private String nombrePropiedad;
 
-    @NonNull
+    @Column(name = "UBICACION", nullable = false)
     private String ubicacion;
 
-    @NonNull
+    @Column(name = "NUM_HABITACIONES", nullable = false)
     private int numHabitaciones;
 
-    @NonNull
+    @Column(name = "NUM_BANOS", nullable = false)
     private int numBanos;
 
-    @NonNull
+    @Column(name = "VALOR_COM", nullable = false)
     private int valorCom;
+
+    public Propiedad(String nombrePropiedad, String ubicacion, int numHabitaciones, int numBanos, int valorCom) {
+        this.nombrePropiedad = nombrePropiedad;
+        this.ubicacion = ubicacion;
+        this.numHabitaciones = numHabitaciones;
+        this.numBanos = numBanos;
+        this.valorCom = valorCom;
     }
+}
+
